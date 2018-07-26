221cc648fc425f946a04c59ab369590f97fcc90c
bastos77200 <boman77@msn.com>
2018-07-27 01:01:00 +0200

    V1.01

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+    //activation du panel contenant les slots
+    bool activation = false;
+    //on recupere le GameObject PAnel
+    GameObject Panel;
+    //On déclare un tableau pour les slots
+    public int[] slot;
+
+
+    private void Start()
+    {
+        GetComponent<Canvas>().enabled = false;
+        //Panel = 1er enfant GameObjectInventory
+        Panel = transform.GetChild(0).gameObject;
+
+        //Les enfants de mon panel
+        slot = new int[Panel.transform.childCount];
+    }
+    void Update()
+    {
+        //Lorsque j'appuie sur I
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            //alors j'affiche mon canvas
+            activation = !activation;
+            GetComponent<Canvas>().enabled = activation;
+        }
+
+        
+    }
+
+    public void UpdateRessource(int numeroSlot, string nbr)
+    {
+        //on recupere l'élément enfant txt du slot
+        Panel.transform.GetChild(numeroSlot).GetChild(1).GetComponent<Text>().text = nbr;
+    }
+}
---
+++ Scripts/SelectObjectInventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObjectInventory : MonoBehaviour {
+    //Déclaration de la variable de type Inventaire
+    Inventory Inventory_script;
+
+
+    private void Start()
+    {
+        //recherche GameObject du nom Inventory et son composant Inventory
+        Inventory_script = GameObject.Find("Inventory").GetComponent<Inventory>();
+    }
+    public void Selection()
+    {
+
+        // Numero de slot
+
+        int numeroSlot = transform.parent.GetSiblingIndex();
+
+        //Décremente de 1 le stock du slot
+        Inventory_script.slot[numeroSlot] -= 1;
+        Inventory_script.UpdateRessource(numeroSlot, Inventory_script.slot[numeroSlot].ToString());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
