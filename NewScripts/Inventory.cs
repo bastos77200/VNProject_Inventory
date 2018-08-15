63b57d161fbea9526418a9438ed6b121fd1aee8a
bastos77200 <boman77@msn.com>
2018-08-15 23:25:10 +0200

    v1.2
    reparti sur de nouvelle base plus simple.
    il faut que je recupere l'element image du game objet pour le mettre dans canvas. Etude en cours. votre avis est le bien venu

---
+++ NewScripts/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    
+
+    public GameObject[] inventory = new GameObject[10];
+
+    public void AddItem(GameObject item)
+    {
+        bool itemAdded = false;
+        
+        //Recherche le premier slot de l'inventaire
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                inventory[i] = item;
+                Debug.Log(item.name + " il est ajouter");
+                itemAdded = true;
+                item.SetActive(false);
+                
+                
+                break;
+            }
+            
+        }
+
+        //Inventaire complet
+        if (!itemAdded)
+        {
+            Debug.Log("Inventaire compet - Item non ajouter");
+            item.SetActive(true);
+        }
+        
+    }
+}
---
+++ NewScripts/MouseInteract.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteract : MonoBehaviour {
+    public Inventory inventory;
+
+
+    // Use this for initialization
+    void Start () {
+        HighLight highlight = GetComponent<HighLight>();
+        if (highlight != null)
+        {
+            highlight.StartHighlight();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //inventory.AddItem();
+      
+    }
+
+    private void OnMouseDown()
+    {
+     
+        Debug.Log("collide");
+
+        inventory.AddItem(gameObject);
+    
+        
+    }
+}
